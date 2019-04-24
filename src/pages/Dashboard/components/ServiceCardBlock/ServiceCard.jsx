26e4a159a7bb713a6e190b275895eeaddda36d57
zhaofeng-shu33 <616545598@qq.com>
2019-04-24 21:33:14 +0800

    render second course

--- src/pages/Dashboard/components/ServiceCardBlock/ServiceCard.jsx
+++ src/pages/Dashboard/components/ServiceCardBlock/ServiceCard.jsx
@@ -29,12 +29,20 @@ export default class ServiceCard extends Component {
           let date = new Date(item.start_time);
           let date_str = this.props.intl.formatDate(date);
           let time_str = this.props.intl.formatTime(date);
+          let second_course_info = null;
+          if (item.start_time_2) {
+            let date_2 = new Date(item.start_time_2);
+            let date_str_2 = this.props.intl.formatDate(date_2);
+            let time_str_2 = this.props.intl.formatTime(date_2);
+            let date_time_str_2 = date_str_2 + ' ' + time_str_2;
+            second_course_info = <p style={styles.desc}>《{item.course_2.name}》<span>上课时间：</span>{date_time_str_2}</p>
+          }
           let class_name = item.course.grade + '年级' +  item.class_id + '班';
           return (
               <Card key={index} title={item.school} extra={class_name}>
                 <div>
-                  <p style={styles.desc}>《{item.course.name}》</p>
-                  <p style={styles.desc}> <span>上课时间：</span>{date_str + ' ' + time_str} </p>
+                <p style={styles.desc}>《{item.course.name}》<span>上课时间：</span>{date_str + ' ' + time_str}</p>
+                {second_course_info}
                 </div>
               <div style={styles.footer}>
                 <a href="#" style={styles.link}>
