1404f1d015107063c14d1aaa79329fb9a2fcdf45
zhaofeng-shu33 <616545598@qq.com>
2019-04-24 20:37:20 +0800

    one item on each row.

--- src/pages/Dashboard/components/ServiceCardBlock/ServiceCard.jsx
+++ src/pages/Dashboard/components/ServiceCardBlock/ServiceCard.jsx
@@ -1,19 +1,7 @@
 import React, { Component } from 'react';
-import { Grid, Icon, Message } from '@alifd/next';
-import IceContainer from '@icedesign/container';
+import { Message, Card } from '@alifd/next';
 import { getClassList } from '../../../../api/class_apply';
-const { Row, Col } = Grid;
 import { injectIntl } from 'react-intl';
-// MOCK 数据，实际业务按需进行替换
-const getData = () => {
-  return Array.from({ length: 6 }).map(() => {
-    return {
-      name: '服务名称',
-      desc: '这里是一段相关的服务简介，介绍产品的功能、特点',
-      tag: '精选',
-    };
-  });
-};
 
 @injectIntl
 export default class ServiceCard extends Component {
@@ -36,72 +24,39 @@ export default class ServiceCard extends Component {
   }
   render() {
     return (
-      <Row wrap gutter="20">
+      <div>
         {this.state.data.map((item, index) => {
           let date = new Date(item.start_time);
           let date_str = this.props.intl.formatDate(date);
           let time_str = this.props.intl.formatTime(date);
+          let class_name = item.course.grade + '年级' +  item.class_id + '班';
           return (
-            <Col l="8" key={index}>
-              <IceContainer style={styles.container}>
-                <div style={styles.body}>
-                  <h5 style={styles.name}>{item.school}</h5>
+              <Card key={index} title={item.school} extra={class_name}>
+                <div>
                   <p style={styles.desc}>《{item.course.name}》</p>
                   <p style={styles.desc}> <span>上课时间：</span>{date_str + ' ' + time_str} </p>
-                  <div style={styles.tag}> {item.course.grade} 年级 {item.class_id} 班</div>
                 </div>
-                <div style={styles.footer}>
-                  <a href="#" style={{ ...styles.link, ...styles.line }}>
-                    <Icon type="office" size="small" style={styles.icon} />
+              <div style={styles.footer}>
+                <a href="#" style={styles.link}>
                     课程介绍
                   </a>
-                  <a href="#" style={styles.link}>
-                    <Icon type="box" size="small" style={styles.icon} />
+                <a href="#" style={styles.link}>
                     认领班级
                   </a>
                 </div>
-              </IceContainer>
-            </Col>
+              </Card>
           );
         })}
-      </Row>
+      </div>
     );
   }
 }
 
 const styles = {
-  container: {
-    padding: '0',
-  },
-  body: {
-    padding: '20px',
-    height: '120px',
-    position: 'relative',
-    borderBottom: '1px solid #f0f0f0',
-  },
-  name: {
-    margin: '0',
-    padding: '0',
-    height: '28px',
-    lineHeight: '28px',
-    fontSize: '16px',
-    color: '#0d1a26',
-  },
   desc: {
     fontSize: '14px',
     color: '#697b8c',
     margin: '12px 0',
-  },
-  tag: {
-    background: '#fff0f6',
-    border: '1px solid #ffadd2',
-    color: '#eb2f96',
-    position: 'absolute',
-    right: '20px',
-    top: '20px',
-    padding: '4px 12px',
-    textAlign: 'center',
-    borderRadius: '50px',
   },
   footer: {
     display: 'flex',
@@ -109,18 +64,10 @@ const styles = {
     justifyContent: 'space-around',
   },
   link: {
-    height: '56px',
-    lineHeight: '56px',
     color: '#314659',
     cursor: 'pointer',
     textDecoration: 'none',
     width: '50%',
     textAlign: 'center',
-  },
-  line: {
-    borderRight: '1px solid #f0f0f0',
-  },
-  icon: {
-    marginRight: '5px',
   },
 };
