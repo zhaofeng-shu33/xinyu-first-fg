c4f202e0d3e28ee5c51407918391ae8b9bc1a205
zhaofeng-shu33 <616545598@qq.com>
2019-09-02 04:02:44 +0800

    do not upload to oss using travis since aliyun domestric oss service is inreachable from overseas

---
+++ upload.sh
@@ -0,0 +1,3 @@
+# wget http://gosspublic.alicdn.com/ossutil/1.5.2/ossutil64
+# chmod 755 ossutil64
+ossutil -u -i LTAIhGvOLG5VLaqh -k $AccessKeySecret -e http://oss-cn-shenzhen.aliyuncs.com cp -r build/ oss://yiqiaifa/
